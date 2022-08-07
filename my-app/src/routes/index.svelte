1e1a6f5e715049aaebff52bb2ea09f15b8d2c53e
Alon Baker <AlonDavid2006@live.com>
2022-08-07 16:08:44 -0400

    first commit

---
+++ my-app/src/routes/index.svelte
@@ -0,0 +1,31 @@
+<script>
+	let name = 'Alon';
+    import Nested from './Nested.svelte';
+    let count = 0;
+
+	function incrementCount() {
+		count += 1;
+	}
+</script>
+
+<style>
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
+
+<h1>Hello {name.toUpperCase()}</h1>
+
+<p>This is a paragraph.</p>
+<Nested answer={"4222"}/>
+<Nested/>
+<Nested answer={"44432"}/>
+<Nested answer={"4342"}/>
+<Nested answer={"432"}/>
+<Nested answer={"342"}/>
+
+<button on:click={incrementCount}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
